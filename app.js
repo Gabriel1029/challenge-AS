f23024fbe934dd9ebbdfbd2b95378bf2389698b6
gabriel1029 <gabocs2020@gmail.com>
2025-01-31 10:56:21 -0500

    2-version

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+
+function agregarAmigo(){
+    let campo = document.getElementById('amigo');
+    if (campo.value === '') {
+        alert('Por Favor, inserte un nombre')
+    }else{
+        amigos.push(campo.value);
+        console.log(amigos);
+        if (campo.value != '') {
+            campo.value = '';
+        }
+    }
+}
